30bad14cdb783f3f843abf851a716a31a73355ae
Radoslaw Zuchowski <radoslaw.zuchowski@allegro.pl>
2021-08-08 12:16:44 +0200

    Initial commit

---
+++ src/main/java/pl/zuchol/wimf/WimfApplication.java
@@ -0,0 +1,13 @@
+package pl.zuchol.wimf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WimfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zuchol/wimf/WimfApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zuchol.wimf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
